f1eda5a080c7cd89122d9e7e543bf199b6010dc2
Heyder <heyderrojas2003@gmail.com>
2023-01-13 11:08:59 -0500

    First home interface

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,18 @@
     <video autoplay loop muted class="bg-video">
       <source src="../assets/Mountains.mp4" type="video/mp4">
     </video>
+
+    <div class="home-text">
+      <img src="@/assets/Logo.png" alt="">
+      <div class="text-container">
+        <div class="text-section">
+          <h3>The travel like a breather for life.</h3>
+          <p>Give me a moment, take a moment...</p>
+        </div>
+        <button>Look at destinations</button>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -13,11 +25,59 @@ export default{
 </script>
 
 <style lang="scss">
+.home{
+  position: relative;
+}
 .bg-video{
   width: 100%;
   max-width: 100%;
   max-height: 100vh;
-  object-fit: fill;
-  z-index: -100;
+  object-fit: cover;
+}
+.home-text{
+  background-color: #000000da;
+  display: flex;
+  position: absolute;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+  height: 100vh;
+  top: 0;
+  img{
+    position: absolute;
+    top: 10%;
+    left: 10%;
+    width: 300px;
+  }
+}
+.text-container{
+  width: 80%;
+  button{
+    color: #EEEEEE;
+    font-size: 30px;
+    text-align: center;
+    border: 3px solid #EEEEEE;
+    background: none;
+    padding: 10px 30px;
+  }
+  button:hover{
+    cursor: pointer;
+    background: #000000ea;
+    border: 3px solid #eeeeee00;
+    color: #b9b9b9;
+    transition: .2s;
+  }
+}
+.text-section{
+  color: #fff;
+  margin-bottom: 60px;
+  h3{
+    font-size: 90px;
+    font-weight: bold;
+  }
+  p{
+    font-weight: 400;
+    font-size: 35px;
+  }
 }
 </style>
